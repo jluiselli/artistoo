c201451da4297adf5234bbe3b4bdf2de53b4ca71
Juliette Luiselli <juliette.luiselli@inria.fr>
2022-03-25 16:39:54 +0100

    sorting plots + new competition plot

--- hpc/processing/plot_competition_results.py
+++ hpc/processing/plot_competition_results.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import sys, os
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 folder = sys.argv[1]
 
@@ -13,7 +15,9 @@ for f in os.listdir(folder):
     try:
         ifs = open('./'+folder+'/'+f+'/competition.txt')
         line = ifs.readline()
+        print(line)
         line = ifs.readline()
+        print(line)
         victories += [int(line)]
         ifs.close()
 
@@ -33,3 +37,63 @@ ax.set_title("Nb of wins for each strain")
 fig.tight_layout()
 fig.savefig(folder+'/processing/competition_results.png')
 plt.close(fig)
+
+
+if os.path.exists(folder+'/competition_time.csv'):
+    df = pd.read_csv(folder+'/competition_time.csv')
+else:
+    print("creating df")
+    df = pd.DataFrame()
+    for f in os.listdir(folder):
+        print(f)
+        k = f.replace('/',' ').split('-')
+        i = iter(k)
+        params = dict(zip(i,i))
+
+        print(params)
+        try:
+            tmp = []
+
+            ifs = open('./'+folder+'/'+f+'/competition_log.txt')
+            line = ifs.readline()
+            
+
+            while line:
+                print(line)
+                time, distr = line.split(";")
+                ones = np.char.count([str(distr)], "1")[0]
+                twos = np.char.count([str(distr)], "2")[0]
+                prop_1 = ones/(ones+twos)
+
+                tmp += [[time,ones,prop_1]]
+            
+                line = ifs.readline()
+            
+            ifs.close()
+
+            tmp_df = pd.DataFrame(tmp, columns=["time","ones","prop_1"])
+
+            for p in params:
+                tmp_df[p] = params[p]
+
+        except:
+            print("reading error for "+f)
+            tmp_df = pd.DataFrame()
+        
+        df = pd.concat([df, tmp_df], sort=False)
+    
+    df.to_csv(folder+'/competition_time.csv')
+
+
+print(df.shape)
+# df = df.sample(frac=0.002) # Otherwise too long to plot
+df["seed"] = df["seed"].astype(str) # To get categorical in hue color
+
+fig, ax = plt.subplots(1, 1, figsize=(15,10))
+sns.lineplot(data=df, x='time',y='prop_1', hue='seed', ax=ax, linewidth=3)
+ax.set_ylim(0,1)
+ax.axhline(y=0.5, color='grey')
+fig.tight_layout()
+fig.savefig(folder+'/processing/competition_time.png')
+plt.close(fig)
+
