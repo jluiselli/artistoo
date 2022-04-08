34710ea062e3443bd904d7240517782de8a67752
Juliette Luiselli <juliette.luiselli@inria.fr>
2022-04-08 12:17:15 +0200

    retrieve processing files from devel

--- hpc/processing/auxiliary/agg_deaths_births.py
+++ hpc/processing/auxiliary/agg_deaths_births.py
@@ -9,24 +9,40 @@ deaths, divisions = pd.DataFrame(), pd.DataFrame()
 
 for f in os.listdir(folder):
     k = f.replace('/',' ').split('-')
-    try:
-        k[-2] = k[-2]+'-'+k[-1]
-    except:
-        pass
-    i = iter(k)
-    params = dict(zip(i,i))
+    i = 0
+    while i < len(k):
+        try:
+            if k[i][-2:]=='1e' or k[i][-2:]=='5e':
+                k[i] = k[i]+ '-' + k[i+1]
+                if i < len(k)-2:
+                    k = k[:i+1]+k[i+2:]
+                i=0
+                continue
+        except:
+            i+=1
+        i+=1
+
+    j = iter(k)
+    params = dict(zip(j,j))
 
     print(f)
     print(params)
     try:   
+        print('./'+folder+'/'+f+"/deaths.txt")
         deaths_tmp = rd.read_deaths_births(fname='./'+folder+'/'+f+"/deaths.txt")
-        divisions_tmp = rd.read_deaths_births(fname='./'+folder+'/'+f+"/divisions.txt")
-        
+        print(deaths_tmp)
+        print(folder+'/'+f+'/deaths.csv')
         for k in params:
             deaths_tmp[k] = float(params[k])
+        deaths_tmp.to_csv(folder+'/'+f+'/deaths.csv', sep=';')
+
+        divisions_tmp = rd.read_deaths_births(fname='./'+folder+'/'+f+"/divisions.txt", ignore=0)
+        
+        for k in params:
             divisions_tmp[k] = float(params[k])
         
-        deaths_tmp.to_csv(folder+'/'+f+'/deaths.csv', sep=';')
+        print(divisions_tmp)
+        print(folder+'/'+f+'/deaths.csv')
         divisions_tmp.to_csv(folder+'/'+f+'/divisions.csv', sep=';')
         
         deaths = pd.concat([deaths, deaths_tmp], sort=False)
